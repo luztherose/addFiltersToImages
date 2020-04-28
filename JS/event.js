0d587ad11357e1380eba124de079b54761a34a9d
Luz De La Rosa <luzdelarosaramirez@gmail.com>
2020-04-28 19:15:14 -0400

    Making improvements

--- JS/event.js
+++ JS/event.js
@@ -37,15 +37,40 @@ cleanFieldButton.addEventListener("click", () => {
 let filterOut = document.querySelector("#imageDiv img");
 let h2Heading = document.querySelector("h2");
 let uploadFile = document.querySelector("#loadImage input");
+let original = document.getElementById("filterOut");
+let inputBlur = document.getElementById("blurRange");
+let inputBright = document.getElementById("brightRange");
+let inputGray = document.getElementById("grayRange");
+let inputSepia = document.getElementById("sepiaRange");
+let inputSaturate = document.getElementById("saturateRange");
 
-filterOut.addEventListener("click", () => {
-    if (filterOut.style.filter != "none") {
-        h2Heading.style.color = "#333";
-        filterOut.style.filter="none";
-    } else {
-        filterOut.style.filter="";
-    }
+inputBlur.addEventListener("change", () => {
+    let rangeValue = document.getElementById("blurRange").value;
+    filterOut.style.filter="blur(" + rangeValue + "px)";
 });
+inputBright.addEventListener("change", () => {
+    let brightValue = document.getElementById("brightRange").value;
+    filterOut.style.filter="brightness(" + brightValue + "%)";
+});
+
+inputGray.addEventListener("change", () => {
+    let grayValue = document.getElementById("grayRange").value;
+    filterOut.style.filter="grayscale(" + grayValue + "%)";
+});
+inputSepia.addEventListener("change", () => {
+    let sepiaValue = document.getElementById("sepiaRange").value;
+    filterOut.style.filter="sepia(" + sepiaValue + "%)";
+});
+inputSaturate.addEventListener("change", () => {
+    inputSaturate.style.color="blue";
+    let saturateValue = document.getElementById("saturateRange").value;
+    filterOut.style.filter="saturate(" +  saturateValue  + ")";
+});
+
+original.addEventListener("click", () => {
+    filterOut.style.filter="";
+});
+
 if (uploadFile != null) {
     uploadFile.addEventListener("change", () => {
         filterOut.src = window.URL.createObjectURL(uploadFile.files[0]);
